dcac0195fdfcf1240d94734b4f564078474846b3
Matheus Luna <srmatheus6162@gmail.com>
2025-10-05 12:00:22 -0300

    Feat: Models, Users for all users of application and yours function in school.

---
+++ Models/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjetoGusmaoFinal.Models
+{
+    public class Roles
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjetoGusmaoFinal.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        private required string Password { get; set; }
+        private required string CPF { get; set; }
+    }
+}
