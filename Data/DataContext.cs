dff1bab9b6465f8852e33faefdac701569fb4aa0
Matheus Luna <srmatheus6162@gmail.com>
2025-10-13 23:35:57 -0300

    Adding DataContext to work the ORM.

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ProjetoGusmaoFinal.Models;
 namespace ProjetoGusmaoFinal.Data
 {
     public class DataContext(DbContextOptions options) : DbContext(options)
     {
+        public DbSet<Users> Users { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)//no Model creating definimos relação e caracteristicas de campos, tipo NOT NULL ou coisa do tipo;
+        {
+                
+        }
     }
 }
