aab6c5a665d2320138d0717208fd39ad73483107
Matheus Luna <srmatheus6162@gmail.com>
2025-10-13 23:39:31 -0300

    Configuring the program.cs to work ORM EntityFramework.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.EntityFrameworkCore;
 using ProjetoGusmaoFinal.Components;
+using ProjetoGusmaoFinal.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var ConnectionString = "server=localhost;user=root;password=luna;database=projectg";
+var serverVersion = new MariaDbServerVersion(new Version(12, 0, 2));
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
@@ -15,6 +18,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+builder.Services.AddDbContext<DataContext>(options => options.UseMySql(ConnectionString, serverVersion));
 
 app.UseHttpsRedirection();
 
