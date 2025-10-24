5209205bd4c9c37e007a442e0d853af98d4c4c5b
Matheus Luna <srmatheus6162@gmail.com>
2025-10-23 23:16:41 -0300

    [T-1] Adding service to hash values

---
+++ Classes/HashResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjetoGusmaoFinal.Classes
+{
+    public class HashResponse
+    {
+        public string Hash { get; set; } = string.Empty;
+        public bool Success { get; set; } = false;
+    }
+}
---
+++ Services/HashService.cs
@@ -0,0 +1,24 @@
+﻿using ProjetoGusmaoFinal.Classes;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ProjetoGusmaoFinal.Services
+{
+    public class HashService
+    {
+        public static HashResponse Crypt(string Value)
+        {
+            HashResponse HashResponse = new();
+            try
+            {
+                byte[] CryptedValue = SHA256.HashData(Encoding.UTF8.GetBytes(Value));
+                HashResponse.Hash = Convert.ToBase64String(CryptedValue);
+                HashResponse.Success = true;
+                return HashResponse;
+            }
+            catch { 
+                return HashResponse;
+            }
+        }
+    }
+}
