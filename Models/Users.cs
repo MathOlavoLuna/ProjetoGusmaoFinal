0c3c54201a342982c5b80059d26d8cf14619c813
Matheus Luna <srmatheus6162@gmail.com>
2025-10-23 23:16:12 -0300

    T-1 - I changed the variables to public for this model to work if isn't it breaks, and I defined the setters and getters manually, with this we can encrypt the data before sending it to the database.

--- Models/Users.cs
+++ Models/Users.cs
@@ -1,13 +1,30 @@
 ﻿namespace ProjetoGusmaoFinal.Models
 {
-    public class Users
+    public class Users(string Password, string CPF)
     {
         public int Id { get; set; }
         public required Roles Role { get; set; }
         public required int RoleID { get; set; }
         public required string Name { get; set; }
         public required string Email { get; set; }
-        private required string Password { get; set; }
-        private required string CPF { get; set; }
+        public string Password;
+        public string CPF;
+
+        public void SetPassword(string Password)
+        {
+            _Password = Password;
+        }
+        public void GetPassword()
+        {
+
+        }
+        public void SetCPF()
+        {
+
+        }
+        public void GetCPF()
+        {
+
+        }
     }
 }
