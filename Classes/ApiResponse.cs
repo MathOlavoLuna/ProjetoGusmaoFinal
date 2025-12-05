4c53ba105ff095c3f95b10926c714bc36e137b4b
Matheus Luna <srmatheus6162@gmail.com>
2025-12-04 23:09:06 -0300

    completed create user

---
+++ Classes/ApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjetoGusmaoFinal.Classes
+{
+    public class ApiResponse<Class> where Class : class
+    {
+        public List<Class> Data { get; set; } = [];
+        public string Message { get; set; } = string.Empty;
+        public bool Success { get; set; } = false;
+    }
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,28 @@
+﻿using ProjetoGusmaoFinal.Classes;
+using ProjetoGusmaoFinal.Data;
+using ProjetoGusmaoFinal.Models;
+
+namespace ProjetoGusmaoFinal.Services
+{
+    public class UserService(CRUDService<Users> Entity)
+    {
+        private readonly CRUDService<Users> _entity = Entity;
+
+        public async Task<ApiResponse<Users>> PostUser(Users User)
+        {
+            ApiResponse<Users> Response = new();
+            try
+            {
+
+                Users CreatedUser = await _entity.Post(User);
+                Response.Data.Add(CreatedUser);
+                Response.Success = true;
+            }
+            catch (Exception Ex)
+            {
+                Response.Message = Ex.Message;
+            }
+            return Response;
+        }
+    }
+}
