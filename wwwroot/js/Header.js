3baa7b5e3415088dd432b886ba4864233f708267
Matheus Luna <srmatheus6162@gmail.com>
2025-12-08 22:46:27 -0300

    [T-3] new adjusting

--- wwwroot/js/Header.js
+++ wwwroot/js/Header.js
@@ -4,10 +4,10 @@ const NavBarTitle = document.getElementById('navbar-title');
 window.addEventListener('scroll', () => {
     for (var i = 0; i < NavBarButtons.length; i++) {
         const Btn = NavBarButtons[i]
-        if (window.scrollY > 500) {
+        if (window.scrollY > 350) {
             NavBarTitle.classList.add('navbar-title-scrollY');
             Btn.classList.add('navbar-option-scrollY');
-        } else if (window.scrollY < 500) {
+        } else if (window.scrollY < 350) {
             Btn.classList.remove('navbar-option-scrollY');
             NavBarTitle.classList.remove('navbar-title-scrollY');
         }
