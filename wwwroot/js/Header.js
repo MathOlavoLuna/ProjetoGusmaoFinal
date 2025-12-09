cfec134ec9971e51de94905460b051dcbb30ea44
Matheus Luna <srmatheus6162@gmail.com>
2025-12-08 22:43:23 -0300

    [T-3] Adjusting navbar

---
+++ wwwroot/js/Header.js
@@ -0,0 +1,16 @@
+﻿const NavBarButtons = document.getElementsByClassName('navbar-option');
+const NavBarTitle = document.getElementById('navbar-title');
+
+window.addEventListener('scroll', () => {
+    for (var i = 0; i < NavBarButtons.length; i++) {
+        const Btn = NavBarButtons[i]
+        if (window.scrollY > 500) {
+            NavBarTitle.classList.add('navbar-title-scrollY');
+            Btn.classList.add('navbar-option-scrollY');
+        } else if (window.scrollY < 500) {
+            Btn.classList.remove('navbar-option-scrollY');
+            NavBarTitle.classList.remove('navbar-title-scrollY');
+        }
+    }
+})
+
