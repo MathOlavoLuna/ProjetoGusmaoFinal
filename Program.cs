f128ee2c860991f19da73d0f2a0ee90f7fb95f31
Matheus Luna <srmatheus6162@gmail.com>
2025-12-25 21:48:08 -0300

    quase completo tela login

--- Program.cs
+++ Program.cs
@@ -1,34 +1,47 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using ProjetoGusmaoFinal.Components;
 using ProjetoGusmaoFinal.Data;
-using ProjetoGusmaoFinal.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
 var serverVersion = new MariaDbServerVersion(new Version(12, 0, 2));
-// Add services to the container.
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-
 builder.Services.AddDbContext<DataContext>(options =>
-options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), serverVersion));
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        serverVersion
+    )
+);
+
+    
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/login";
+        options.AccessDeniedPath = "/acesso-negado";
+    });
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-
-
-// Configure the HTTP request pipeline.
+// Pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
