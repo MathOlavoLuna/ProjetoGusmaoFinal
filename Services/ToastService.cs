64daa707f18cc26b9acce6ac1bc9174ab1344fa5
Matheus Luna <matheus@in4y.com.br>
2026-01-07 17:17:39 -0300

    desisto

---
+++ Services/ToastService.cs
@@ -0,0 +1,23 @@
+﻿namespace ProjetoGusmaoFinal.Services
+{
+    public class ToastService
+    {
+        public event Action? OnChange;
+
+        public string Message { get; private set; } = string.Empty;
+        public bool Success { get; private set; }
+
+        public async Task Show(string message, bool success, int duration = 2000)
+        {
+            Message = message;
+            Success = success;
+            OnChange?.Invoke();
+
+            await Task.Delay(duration);
+
+            Message = string.Empty;
+            OnChange?.Invoke();
+        }
+    }
+
+}
